9411e32835ea632f03664d68ad8b6a991819cf5a
jakeheke75 <jakeheke75@gmail.com>
2023-05-12 22:48:24 +0200

    update

--- loader.py
+++ loader.py
@@ -1,15 +1,3 @@
-import json
-
-FILENAME = 'coinbase.txt'
-
-# parse the snapshot message
-def parse_snapshot(ws_data):
-    print(ws_data);
-
-# parse the ws message and understand what to do
-def parse_message(ws_data):
-    if ws_data['type'] == 'snapshot':
-        parse_snapshot(ws_data) 
 
 # read the data file
 with open(FILENAME) as file:
