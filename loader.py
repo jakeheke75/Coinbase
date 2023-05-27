a05274a8e0c72a3344c1a768222b867f5e2ff3a7
jakeheke75 <jakeheke75@gmail.com>
2023-05-28 00:17:29 +0200

    adding sortedcontainers

--- loader.py
+++ loader.py
@@ -1,16 +1,21 @@
 import json
+from sortedcontainers import SortedDict
+
 FILENAME = 'coinbase.txt'
 
 # parse the snapshot message
 def parse_snapshot(ws_data):
-    print(ws_data)
+    for ask in ws_data['asks']:
+        print(ask[0])
+    for bid in ws_data['bids']:
+        print(bid[0])
 
 # parse the ws message and understand what to do
-def parse_message(ws_data):
+def parse_coinbase(ws_data):
     if ws_data['type'] == 'snapshot':
         parse_snapshot(ws_data)
 
 # read the data file
 with open(FILENAME) as file:
     while line := file.readline():
-        parse_message(json.loads(line))
+        parse_coinbase(json.loads(line))
